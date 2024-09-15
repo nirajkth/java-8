f414945463ed74164fb99e81c69a4db741fa33f8
Niraj Kumar <nirajth.k1@gmail.com>
2024-09-15 10:38:57 +0530

    Person PersonRepository

---
+++ src/com/java8/repo/PersonRepository.java
@@ -0,0 +1,19 @@
+package com.java8.repo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PersonRepository {
+    public static List<Person> getPersons() {
+        Person p1 = new Person("Ram", 190, 100000.00, "Male", 6,
+                Arrays.asList("Kabaddi", "Chess", "Badminton"));
+        Person p2 = new Person("Shyam", 189, 90000.00, "Male", 4,
+                Arrays.asList("L", "Chess", "Judo"));
+        Person p3 = new Person("Rukmini", 160, 60000.00, "Female", 10,
+                Arrays.asList("Polo", "Kabaddi", "Judo"));
+        Person p4 = new Person("Radha", 220, 300000.00, "Female", 2,
+                Arrays.asList("Judo", "Polo", "Kabaddi"));
+
+        return Arrays.asList(p1, p2, p3, p4);
+    }
+}
