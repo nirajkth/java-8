aacfd2a0fbe8f4721ba1befc4f251e5a91540f25
Niraj Kumar <nirajth.k1@gmail.com>
2024-09-15 18:09:04 +0530

    Consumer used with for each

--- src/com/java8/comsumer/PersonConsumer.java
+++ src/com/java8/comsumer/PersonConsumer.java
@@ -1,11 +1,13 @@
 package com.java8.comsumer;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 import com.java8.repo.Person;
 import com.java8.repo.PersonRepository;
 
 public class PersonConsumer {
+    static List<Person> persons = PersonRepository.getPersons();
 
     static Consumer<Person> consumer1 = person -> System.out.println(person);
     static Consumer<Person> consumer2 = person -> System.out.println(person.getName().toUpperCase());
@@ -17,6 +19,26 @@ public class PersonConsumer {
         // consumer2.accept(person);
         // consumer3.accept(person);
 
-        consumer1.andThen(consumer2).andThen(consumer3).accept(person);
+        // consumer1.andThen(consumer2).andThen(consumer3).accept(person);
+
+        printNameAndHobbies();
+        printWithCondition();
     }
+
+    public static void printNameAndHobbies() {
+        persons.forEach(consumer2.andThen(consumer3));
+    }
+
+    public static void printWithCondition() {
+        System.out.println("----------------");
+        persons.forEach(person -> {
+            boolean isMale = person.getGender().equalsIgnoreCase("Male");
+            boolean isHeightGreatherThan140 = person.getHeight() > 140;
+
+            if (isMale && isHeightGreatherThan140) {
+                consumer2.andThen(consumer3).accept(person);
+            }
+        });
+    }
+
 }
--- src/com/java8/repo/PersonRepository.java
+++ src/com/java8/repo/PersonRepository.java
@@ -8,7 +8,7 @@ public class PersonRepository {
                 Person p1 = new Person("Ram", 190, 100000.00, "Male", 6,
                                 Arrays.asList("Kabaddi", "Chess", "Badminton"));
                 Person p2 = new Person("Shyam", 189, 90000.00, "Male", 4,
-                                Arrays.asList("L", "Chess", "Judo"));
+                                Arrays.asList("Hockey", "Chess", "Judo"));
                 Person p3 = new Person("Rukmini", 160, 60000.00, "Female", 10,
                                 Arrays.asList("Polo", "Kabaddi", "Judo"));
                 Person p4 = new Person("Radha", 220, 300000.00, "Female", 2,
