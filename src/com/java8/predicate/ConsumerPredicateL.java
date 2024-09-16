dc7bc1558cacf8b1605ed8c02d5e884f43ee0631
Niraj Kumar <nirajth.k1@gmail.com>
2024-09-16 11:26:34 +0530

    Consumer Predicate example

---
+++ src/com/java8/predicate/ConsumerPredicateL.java
@@ -0,0 +1,32 @@
+package com.java8.predicate;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+import com.java8.repo.Person;
+import com.java8.repo.PersonRepository;
+
+public class ConsumerPredicateL {
+    static Predicate<Person> p1 = per -> per.getHeight() >= 140;
+    static Predicate<Person> p2 = per -> per.getGender().equals("Male");
+
+    static BiPredicate<Integer, String> p3 = (height, gender) -> height >= 140 && gender.equals("Male");
+
+    static BiConsumer<String, List<String>> hobbiesConsumer = (name, hobbies) -> System.out.println(name + hobbies);
+
+    static Consumer<Person> personConsumer = per -> {
+        // if (p1.and(p2).test(per)) {
+        if (p3.test(per.getHeight(), per.getGender())) {
+            hobbiesConsumer.accept(per.getName(), per.getHobbies());
+        }
+    };
+
+    public static void main(String[] args) {
+        List<Person> persons = PersonRepository.getPersons();
+        persons.forEach(personConsumer);
+    }
+}
